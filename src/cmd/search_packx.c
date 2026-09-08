f0ca9f4f0a1d71fc53d63c83ad367ea8077508b8
wxxden01 <rudy0017@icloud.com>
2026-09-08 09:06:58 +0200

    fix commande search

--- src/cmd/search_packx.c
+++ src/cmd/search_packx.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// contient la fonction package_existe()
+#include "has_sudo.h"
 #include "packages.h"
 
 #define PATH_MAX_LEN 256
 
 int packx_search(int argc, char **argv)
 {
-    if (argc != 3)
+    if ((sudo(argv[0]) && argc != 4) || (!sudo(argv[0]) && argc != 3))
     {
-        package_t pkg;
-        if (pkg_finder(1, argv[2], &pkg) == 0)
-        {
-            printf("Le paquet %s à bien été trouvé!\n", argv[2]);
-            return 0;
-        }
-        printf("Le paquet %s n'a pas été trouvé!\n", argv[2]);
-        return 0;
+        printf("Mauvaise utilisation de la commande! 'packx search <pkg>'\n");
+        return -1;
     }
-    printf("Mauvaise utilisation de la commande! 'packx search <pkg>'\n");
-    return 1;
+
+    char pkg_selected[256];
+
+        // Check if argc has enough elements before accessing argv
+        if (sudo(argv[0])) {
+            strncpy(pkg_selected, argv[3], sizeof(pkg_selected) - 1);
+            pkg_selected[sizeof(pkg_selected) - 1] = '\0';
+        } else {
+            strncpy(pkg_selected, argv[2], sizeof(pkg_selected) - 1);
+            pkg_selected[sizeof(pkg_selected) - 1] = '\0';
+        }
+
+    package_t pkg;
+    if (pkg_finder(1, pkg_selected, &pkg) != 0)
+    {
+        printf("Le paquet %s n'a pas été trouvé!\n", pkg_selected);
+        return -1;
+    }
+    printf("Le paquet %s à bien été trouvé!\n", pkg_selected);
+
+    return 0;
 }
